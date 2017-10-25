a36369da4e85993ba047ea815b2b7118e39fa3c5
Jonh <jonathan@laboratoria.la>
2017-10-24 22:57:13 -0500

    si

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var comparacion = function(array1, array2){
 	}
 }
 
-var arreglo1 = [1,2,3,4,5,6,7,8,9,0];
-var arreglo2 = [1,2,3,4,5,6,7,8,9,0];
+var arreglo1 = [1,2,3,4,5,6,7,8,9,1];
+var arreglo2 = [1,2,3,4,5,6,7,8,9,1];
 
 
 comparacion(arreglo1,arreglo2);
